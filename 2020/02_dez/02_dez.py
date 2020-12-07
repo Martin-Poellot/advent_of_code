5a63591beb8da7fb42494465a7e72f4186043849
Martin-Poellot <martin.poellot@googlemail.com>
2020-12-07 17:21:01 +0100

    Create 02_dez.py

---
+++ 2020/02_dez/02_dez.py
@@ -0,0 +1,23 @@
+data = []
+valid = 0
+v = 0
+with open("02_dez_input.txt", 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        part1, pw = line.split(':')
+        freq, letter = part1.split(' ')
+        min_freq, max_freq = freq.split('-')
+        data.append([min_freq, max_freq, letter, pw])
+        c = pw.count(letter)
+        if int(min_freq) <= c <= int(max_freq):
+            valid += 1
+        l1 = pw[int(min_freq)]
+        l2 = pw[int(max_freq)]
+        if l1 is letter or l2 is letter:
+            if l1 is not l2:
+                if l1 is letter:
+                    v += 1
+                elif l2 is letter:
+                    v += 1
+print(f'Result 1: {valid}')
+print(f'Result 2: {v}')
